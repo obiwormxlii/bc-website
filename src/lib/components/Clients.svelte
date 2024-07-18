95647f377fbf44f623acad058f9452b07337c2e3
Bailey Brayton <obiwan998@gmail.com>
2024-07-18 17:33:22 -0400

    fix double css selector

--- src/lib/components/Clients.svelte
+++ src/lib/components/Clients.svelte
@@ -9,7 +9,7 @@
 	];
 </script>
 
-<div class="px-4 lg:px-6 lg:px-8">
+<div class="px-4 lg:px-6">
 	<div
 		class="relative py-6 md:py-10 overflow-hidden before:absolute before:top-0 before:start-0 before:z-10 before:w-20 before:h-full before:bg-gradient-to-r before:from-white before:to-transparent after:absolute after:top-0 after:end-0 after:w-20 after:h-full after:bg-gradient-to-l after:from-white after:to-transparent dark:before:from-neutral-900 dark:after:from-neutral-900"
 	>
