d8cda58a233be56d15d3653d7774d45df392dc70
Bailey Brayton <obiwan998@gmail.com>
2024-07-18 18:31:08 -0400

    link CTA button to contact

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -16,7 +16,7 @@
 			<div class="mt-7 grid gap-3 w-full md:inline-flex">
 				<a
 					class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-					href="#"
+					href="#contact"
 				>
 					Book an appointment
 					<svg
